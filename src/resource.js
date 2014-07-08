932a709b7a81cfa738a1d7c295ee44d5157b6d2d
faaust <link.tank.r@gmail.com>
2014-07-08 13:02:55 +0200

    Added RapidGame Example

--- src/resource.js
+++ src/resource.js
@@ -1,14 +1,20 @@
 var res = {
-    HelloWorld_png : "res/HelloWorld.png",
-    CloseNormal_png : "res/CloseNormal.png",
-    CloseSelected_png : "res/CloseSelected.png"
+    ballpng : "res/Ball.png",
+    dolcevitafont : "res/DolceVita.ttf",
+    intromp3 : "res/intro.mp3",
+    logopng : "res/Logo.png",
+    songmp3 : "res/Song.mp3",
+    wallmp3 : "res/Wall.mp3"
 };
 
 var g_resources = [
     //image
-    res.HelloWorld_png,
-    res.CloseNormal_png,
-    res.CloseSelected_png
+    res.ballpng,
+    res.dolcevitafont,
+    res.intromp3,
+    res.logopng,
+    res.songmp3,
+    res.wallmp3
 
     //plist
 
